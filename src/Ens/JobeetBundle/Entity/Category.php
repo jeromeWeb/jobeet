2a40295b2c349fcbdf2af36c8a38027e15bd71eb
jeromeweb <jerome.weber@stadline.com>
2013-02-18 23:48:27 +0100

    [3] Le modèle de données
    Reste la génération du CRUD

---
+++ src/Ens/JobeetBundle/Entity/Category.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace Ens\JobeetBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Category
+ */
+class Category
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var \Doctrine\Common\Collections\Collection
+     */
+    private $jobs;
+
+    /**
+     * @var \Doctrine\Common\Collections\Collection
+     */
+    private $category_affiliates;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->jobs = new \Doctrine\Common\Collections\ArrayCollection();
+        $this->category_affiliates = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+    
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Category
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Add jobs
+     *
+     * @param \Ens\JobeetBundle\Entity\Job $jobs
+     * @return Category
+     */
+    public function addJob(\Ens\JobeetBundle\Entity\Job $jobs)
+    {
+        $this->jobs[] = $jobs;
+    
+        return $this;
+    }
+
+    /**
+     * Remove jobs
+     *
+     * @param \Ens\JobeetBundle\Entity\Job $jobs
+     */
+    public function removeJob(\Ens\JobeetBundle\Entity\Job $jobs)
+    {
+        $this->jobs->removeElement($jobs);
+    }
+
+    /**
+     * Get jobs
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getJobs()
+    {
+        return $this->jobs;
+    }
+
+    /**
+     * Add category_affiliates
+     *
+     * @param \Ens\JobeetBundle\Entity\CategoryAffiliate $categoryAffiliates
+     * @return Category
+     */
+    public function addCategoryAffiliate(\Ens\JobeetBundle\Entity\CategoryAffiliate $categoryAffiliates)
+    {
+        $this->category_affiliates[] = $categoryAffiliates;
+    
+        return $this;
+    }
+
+    /**
+     * Remove category_affiliates
+     *
+     * @param \Ens\JobeetBundle\Entity\CategoryAffiliate $categoryAffiliates
+     */
+    public function removeCategoryAffiliate(\Ens\JobeetBundle\Entity\CategoryAffiliate $categoryAffiliates)
+    {
+        $this->category_affiliates->removeElement($categoryAffiliates);
+    }
+
+    /**
+     * Get category_affiliates
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getCategoryAffiliates()
+    {
+        return $this->category_affiliates;
+    }
+}
---
+++ src/Ens/JobeetBundle/Entity/Job.php
@@ -0,0 +1,529 @@
+<?php
+
+namespace Ens\JobeetBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Job
+ */
+class Job
+{
+	/**
+	 * @var string
+	 */
+	private $id;
+
+	/**
+	 * @var string
+	 */
+	private $generator;
+
+	/**
+	 * @var string
+	 */
+	private $type;
+
+	/**
+	 * @var string
+	 */
+	private $company;
+
+	/**
+	 * @var string
+	 */
+	private $logo;
+
+	/**
+	 * @var string
+	 */
+	private $url;
+
+	/**
+	 * @var string
+	 */
+	private $position;
+
+	/**
+	 * @var string
+	 */
+	private $location;
+
+	/**
+	 * @var string
+	 */
+	private $description;
+
+	/**
+	 * @var string
+	 */
+	private $how_to_apply;
+
+	/**
+	 * @var string
+	 */
+	private $token;
+
+	/**
+	 * @var boolean
+	 */
+	private $is_public;
+
+	/**
+	 * @var boolean
+	 */
+	private $is_activated;
+
+	/**
+	 * @var string
+	 */
+	private $email;
+
+	/**
+	 * @var \DateTime
+	 */
+	private $expires_at;
+
+	/**
+	 * @var \DateTime
+	 */
+	private $created_at;
+
+	/**
+	 * @var \DateTime
+	 */
+	private $updated_at;
+
+	/**
+	 * @var \Ens\JobeetBundle\Entity\Category
+	 */
+	private $category;
+
+
+	/**
+	 * Set id
+	 *
+	 * @param string $id
+	 * @return Job
+	 */
+	public function setId($id)
+	{
+		$this->id = $id;
+
+		return $this;
+	}
+
+	/**
+	 * Get id
+	 *
+	 * @return string
+	 */
+	public function getId()
+	{
+		return $this->id;
+	}
+
+	/**
+	 * Set generator
+	 *
+	 * @param string $generator
+	 * @return Job
+	 */
+	public function setGenerator($generator)
+	{
+		$this->generator = $generator;
+
+		return $this;
+	}
+
+	/**
+	 * Get generator
+	 *
+	 * @return string
+	 */
+	public function getGenerator()
+	{
+		return $this->generator;
+	}
+
+	/**
+	 * Set type
+	 *
+	 * @param string $type
+	 * @return Job
+	 */
+	public function setType($type)
+	{
+		$this->type = $type;
+
+		return $this;
+	}
+
+	/**
+	 * Get type
+	 *
+	 * @return string
+	 */
+	public function getType()
+	{
+		return $this->type;
+	}
+
+	/**
+	 * Set company
+	 *
+	 * @param string $company
+	 * @return Job
+	 */
+	public function setCompany($company)
+	{
+		$this->company = $company;
+
+		return $this;
+	}
+
+	/**
+	 * Get company
+	 *
+	 * @return string
+	 */
+	public function getCompany()
+	{
+		return $this->company;
+	}
+
+	/**
+	 * Set logo
+	 *
+	 * @param string $logo
+	 * @return Job
+	 */
+	public function setLogo($logo)
+	{
+		$this->logo = $logo;
+
+		return $this;
+	}
+
+	/**
+	 * Get logo
+	 *
+	 * @return string
+	 */
+	public function getLogo()
+	{
+		return $this->logo;
+	}
+
+	/**
+	 * Set url
+	 *
+	 * @param string $url
+	 * @return Job
+	 */
+	public function setUrl($url)
+	{
+		$this->url = $url;
+
+		return $this;
+	}
+
+	/**
+	 * Get url
+	 *
+	 * @return string
+	 */
+	public function getUrl()
+	{
+		return $this->url;
+	}
+
+	/**
+	 * Set position
+	 *
+	 * @param string $position
+	 * @return Job
+	 */
+	public function setPosition($position)
+	{
+		$this->position = $position;
+
+		return $this;
+	}
+
+	/**
+	 * Get position
+	 *
+	 * @return string
+	 */
+	public function getPosition()
+	{
+		return $this->position;
+	}
+
+	/**
+	 * Set location
+	 *
+	 * @param string $location
+	 * @return Job
+	 */
+	public function setLocation($location)
+	{
+		$this->location = $location;
+
+		return $this;
+	}
+
+	/**
+	 * Get location
+	 *
+	 * @return string
+	 */
+	public function getLocation()
+	{
+		return $this->location;
+	}
+
+	/**
+	 * Set description
+	 *
+	 * @param string $description
+	 * @return Job
+	 */
+	public function setDescription($description)
+	{
+		$this->description = $description;
+
+		return $this;
+	}
+
+	/**
+	 * Get description
+	 *
+	 * @return string
+	 */
+	public function getDescription()
+	{
+		return $this->description;
+	}
+
+	/**
+	 * Set how_to_apply
+	 *
+	 * @param string $howToApply
+	 * @return Job
+	 */
+	public function setHowToApply($howToApply)
+	{
+		$this->how_to_apply = $howToApply;
+
+		return $this;
+	}
+
+	/**
+	 * Get how_to_apply
+	 *
+	 * @return string
+	 */
+	public function getHowToApply()
+	{
+		return $this->how_to_apply;
+	}
+
+	/**
+	 * Set token
+	 *
+	 * @param string $token
+	 * @return Job
+	 */
+	public function setToken($token)
+	{
+		$this->token = $token;
+
+		return $this;
+	}
+
+	/**
+	 * Get token
+	 *
+	 * @return string
+	 */
+	public function getToken()
+	{
+		return $this->token;
+	}
+
+	/**
+	 * Set is_public
+	 *
+	 * @param boolean $isPublic
+	 * @return Job
+	 */
+	public function setIsPublic($isPublic)
+	{
+		$this->is_public = $isPublic;
+
+		return $this;
+	}
+
+	/**
+	 * Get is_public
+	 *
+	 * @return boolean
+	 */
+	public function getIsPublic()
+	{
+		return $this->is_public;
+	}
+
+	/**
+	 * Set is_activated
+	 *
+	 * @param boolean $isActivated
+	 * @return Job
+	 */
+	public function setIsActivated($isActivated)
+	{
+		$this->is_activated = $isActivated;
+
+		return $this;
+	}
+
+	/**
+	 * Get is_activated
+	 *
+	 * @return boolean
+	 */
+	public function getIsActivated()
+	{
+		return $this->is_activated;
+	}
+
+	/**
+	 * Set email
+	 *
+	 * @param string $email
+	 * @return Job
+	 */
+	public function setEmail($email)
+	{
+		$this->email = $email;
+
+		return $this;
+	}
+
+	/**
+	 * Get email
+	 *
+	 * @return string
+	 */
+	public function getEmail()
+	{
+		return $this->email;
+	}
+
+	/**
+	 * Set expires_at
+	 *
+	 * @param \DateTime $expiresAt
+	 * @return Job
+	 */
+	public function setExpiresAt($expiresAt)
+	{
+		$this->expires_at = $expiresAt;
+
+		return $this;
+	}
+
+	/**
+	 * Get expires_at
+	 *
+	 * @return \DateTime
+	 */
+	public function getExpiresAt()
+	{
+		return $this->expires_at;
+	}
+
+	/**
+	 * Set created_at
+	 *
+	 */
+	public function setCreatedAt()
+	{
+		if(!$this->getCreatedAt())
+		{
+			$this->created_at = new \DateTime();
+		}
+	}
+
+	/**
+	 * Get created_at
+	 *
+	 * @return \DateTime
+	 */
+	public function getCreatedAt()
+	{
+		return $this->created_at;
+	}
+
+	/**
+	 * Set updated_at
+	 *
+	 */
+	public function setUpdatedAt()
+	{
+		$this->updated_at = new \DateTime();
+	}
+
+	/**
+	 * Get updated_at
+	 *
+	 * @return \DateTime
+	 */
+	public function getUpdatedAt()
+	{
+		return $this->updated_at;
+	}
+
+	/**
+	 * Set category
+	 *
+	 * @param \Ens\JobeetBundle\Entity\Category $category
+	 * @return Job
+	 */
+	public function setCategory(\Ens\JobeetBundle\Entity\Category $category = null)
+	{
+		$this->category = $category;
+
+		return $this;
+	}
+
+	/**
+	 * Get category
+	 *
+	 * @return \Ens\JobeetBundle\Entity\Category
+	 */
+	public function getCategory()
+	{
+		return $this->category;
+	}
+	/**
+	 * @ORM\PrePersist
+	 */
+	public function setCreatedAtValue()
+	{
+		if(!$this->getCreatedAt())
+		{
+			$this->created_at = new \DateTime();
+		}
+	}
+
+	/**
+	 * @ORM\PreUpdate
+	 */
+	public function setUpdatedAtValue()
+	{
+		$this->updated_at = new \DateTime();
+	}
+}
